12375166f671ede91a3b95ef0bda0e482803570d
Qabir Qabir <qabirrr@gmail.com>
2025-04-07 02:16:29 +0800

    -

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,6 @@
 from fpdf import FPDF
 
-def GetTitle() -> str:
-    pdf_title = input("Enter title for PDF: ")
-    return pdf_title
-
-def CreatePDF(title: str):
+def Main():
     # PDF object
     pdf = FPDF('P', 'mm', 'Letter')
 
@@ -15,15 +11,10 @@ def CreatePDF(title: str):
     pdf.set_font('helvetica', '', 16)
 
     # Add text
-    pdf.cell(40, 10, 'Helslo, world!')
+    pdf.cell(40, 10, 'Hello, world!')
 
     # Create PDF file
-    fileName = title + '.pdf'
-    pdf.output(fileName)
-
-def Main():
-    title = GetTitle()
-    CreatePDF(title)
+    pdf.output('PDF.pdf')
 
 if __name__ == '__main__':
     Main()

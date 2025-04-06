3a140d9ce9c3a533c5390c0d1ab2c4279293157a
Qabir Qabir <qabirrr@gmail.com>
2025-04-07 02:11:10 +0800

    Made a PDF that prints "Hello World"
    Followed tutorial from this video: https://youtu.be/q70xzDG6nls?list=PLjNQtX45f0dR9K2sMJ5ad9wVjqslNBIC0

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,20 @@
-def Main():
-    print("Hey, guys!")
+from fpdf import FPDF
 
-if __name__ == "__main__":
+def Main():
+    # PDF object
+    pdf = FPDF('P', 'mm', 'Letter')
+
+    # Add a page
+    pdf.add_page()
+
+    # Specify font
+    pdf.set_font('helvetica', '', 16)
+
+    # Add text
+    pdf.cell(40, 10, 'Hello, world!')
+
+    # Create PDF file
+    pdf.output('PDF.pdf')
+
+if __name__ == '__main__':
     Main()
